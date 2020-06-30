a87a8160df118da3cc5c7da3e0bc1a18e6ae3c2d
AI1411 <goethe0719@gmail.com>
2020-06-30 16:34:52 +0900

    client追加

---
+++ client/src/main.js
@@ -0,0 +1,49 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify';
+import {ApolloClient} from 'apollo-client';
+import {setContext} from 'apollo-link-context';
+import {createHttpLink} from 'apollo-link-http';
+import {InMemoryCache} from "apollo-cache-inmemory";
+
+import VueApollo from 'vue-apollo';
+
+Vue.config.productionTip = false
+
+Vue.use(VueApollo);
+
+const httpLink = createHttpLink({
+  uri: "http://127.0.0.1:8000/graphql"
+});
+
+const authLink = setContext((_, {headers}) => {
+  const token = localStorage.getItem("vue_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ""
+    }
+  }
+});
+
+const apolloClient = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+  errorHandler(error) {
+    store.commit("error");
+  }
+})
+
+new Vue({
+  router,
+  store,
+  apolloProvider,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
---
+++ client/src/views/Signup.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>アカウント作成ページ</div>
+</template>
+
+<script>
+    export default {
+        name: "Signup"
+    }
+</script>
+
+<style scoped>
+
+</style>
